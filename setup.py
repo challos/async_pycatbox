0c664a7f5d0f999778d95d84d7b3232c77a40478
Andrew McGrail <andrewjerrismcgrail@gmail.com>
2022-06-06 16:13:34 -0400

    Fixed more bugs...

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+version = "1.1"
+
 setup(
     name="async_pycatbox",
-    version="1.0",
+    version=version,
     py_modules=["async_pycatbox"],
     author="Andrew McGrail",
     author_email="andrewjerrismcgrail@gmail.com",
